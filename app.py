e160ab974ccc7a90c558ef7f43c2bfc49ea9d899
toot26201-hash <toot26201@gmail.com>
2026-05-27 17:26:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-⚽ TootScouting: لوحة تحليل الموسم الكاملة
-TypeError: This app has encountered an error. The original error message is redacted to prevent data leaks. Full error details have been recorded in the logs (if you're on Streamlit Cloud, click on 'Manage app' in the lower right of your app).
-Traceback:
-File "/mount/src/tootscouting-dashboard-pc/app.py", line 34, in <module>
-    players = sorted(df['Player'].unique())
+File "/mount/src/tootscouting-dashboard-pc/app.py", line 1
+  ⚽ TootScouting: لوحة تحليل الموسم الكاملة
+  ^
+SyntaxError: invalid character '⚽' (U+26BD)
